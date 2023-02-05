284e44e1f713461cc2e90205d5a07f9ec112954e
artymiz <artyang.ca@gmail.com>
2023-02-05 11:54:32 -0800

    clean up code

--- Assets/Scripts/Player/ShootArea.cs
+++ Assets/Scripts/Player/ShootArea.cs
@@ -1,30 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 public class ShootArea : MonoBehaviour
 {
     [SerializeField] private GameObject crosshair;
-    private float offsetScaleFactor = 0.05f;
+    private const float offsetScaleFactor = 0.1f;
+    private const float resizeScaleFactor = 1.5f;
+    private const float resizeMinValue = 0.3f;
+    private const float crosshairUpdateRate = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(raycastToMousePos(crosshairUpdateRate));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator raycastToMousePos(float freq)
     {
-        Vector3 mousePos = Input.mousePosition;
-        float offsetX = (mousePos.x - Screen.width / 2);
-        float offsetZ = (mousePos.y - Screen.height / 2);
-        float dist = offsetX*offsetX + offsetZ*offsetZ;
-        Vector3 hoverPos = transform.position + new Vector3(
-            offsetScaleFactor * offsetX, 0f, offsetScaleFactor * offsetZ);
-        RaycastHit hit;
-        
-        if (Physics.Raycast(hoverPos, Vector3.down, out hit))
+        while (true)
         {
-            crosshair.transform.position = hit.point;
+            Vector3 mousePos = Input.mousePosition;
+            float offsetX = offsetScaleFactor * (mousePos.x - Screen.width / 2);
+            float offsetZ = offsetScaleFactor * (mousePos.y - Screen.height / 2);
+            Vector3 hoverPos = transform.position + new Vector3(offsetX, 0f, offsetZ);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(hoverPos, Vector3.down, out hit))
+            {
+                crosshair.transform.position = hit.point + Vector3.up;
+                float dist = offsetX*offsetX + offsetZ*offsetZ;
+                float scaleBy = resizeScaleFactor * Mathf.Log10(dist * resizeScaleFactor) + resizeMinValue;
+                crosshair.transform.localScale = new Vector3(scaleBy, 1f, scaleBy);
+            }
+
+            yield return new WaitForSeconds(1f / freq);
         }
-        
     }
 }
